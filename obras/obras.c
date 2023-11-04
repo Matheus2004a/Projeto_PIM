72b352f7e2552eec1ff119ce380218f2e314edc6
Matheus Aurélio <matheusaurelio2004@gmail.com>
2023-11-03 21:19:52 -0300

    remove construction register on file main

--- obras/obras.c
+++ obras/obras.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_OBRAS 100
+
+typedef struct
+{
+  char titulo[100];
+  char descricao[400];
+  char autor[50];
+  int ano;
+} Obra;
+
+Obra obras[MAX_OBRAS];
+int numObras = 0;
+
+void cadastrarObra()
+{
+  if (numObras < MAX_OBRAS)
+  {
+    printf("Digite o título da obra: ");
+    getchar(); // Limpar o buffer de entrada
+    fgets(obras[numObras].titulo, sizeof(obras[numObras].titulo), stdin);
+
+    printf("Digite o descrição da obra: ");
+    getchar();
+    fgets(obras[numObras].descricao, sizeof(obras[numObras].descricao), stdin);
+
+    printf("Digite o autor da obra: ");
+    fgets(obras[numObras].autor, sizeof(obras[numObras].autor), stdin);
+
+    printf("Digite o ano da obra: ");
+    scanf("%d", &obras[numObras].ano);
+
+    numObras++;
+  }
+  else
+  {
+    printf("Limite de obras atingido!\n");
+  }
+}
+
 int administraObras()
 {
   int selecao;
